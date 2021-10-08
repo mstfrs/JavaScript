ae40b44763f559c35eea0b1bf2ffce0e011d6f0f
mstfrs <mstfrs68@gmail.com>
2021-10-08 16:29:41 +0300

    Reverseword dosyası eklendi

--- Loops/app.js
+++ Loops/app.js
@@ -13,14 +13,23 @@
 
 
 
-for (let i = 0; i <= 100 ; i++) {
-    if (i % 5 == 0 && i % 3 == 0){
-        console.log(i,"fizzbuzz");
-    }
-    else if (i % 5 == 0){
-        console.log(i,"buzz");
-    }
-    else if (i % 3 == 0 ){
-        console.log(i,"fizz");
-    }
+// for (let i = 0; i <= 100 ; i++) {
+//     if (i % 5 == 0 && i % 3 == 0){
+//         console.log(i,"fizzbuzz");
+//     }
+//     else if (i % 5 == 0){
+//         console.log(i,"buzz");
+//     }
+//     else if (i % 3 == 0 ){
+//         console.log(i,"fizz");
+//     }
+// }
+
+let myword="M";
+let lastword="";
+
+for (i = myword.length-1; i >= 0; i--) {
+    lastword +=myword[i];
+    
 }
+console.log(lastword.toUpperCase());
