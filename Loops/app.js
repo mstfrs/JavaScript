7ec3d9eda32d871275f941e7a9b35a674c2158cc
mstfrs <mstfrs68@gmail.com>
2021-10-11 15:46:10 +0300

    JS Strings örnekleri eklendi

--- Loops/app.js
+++ Loops/app.js
@@ -25,11 +25,30 @@
 //     }
 // }
 
-let myword="M";
-let lastword="";
+// let myword="M";
+// let lastword="";
 
-for (i = myword.length-1; i >= 0; i--) {
-    lastword +=myword[i];
+// for (i = myword.length-1; i >= 0; i--) {
+//     lastword +=myword[i];
     
-}
-console.log(lastword.toUpperCase());
+// }
+// console.log(lastword.toUpperCase());
+
+var s = "Hello John, welcome to Clarusway.";
+
+// console.log (s.search("Clarusway"));
+// console.log (s.indexOf("Clarusway"));
+
+// console.log (s.slice(0, 5));
+// console.log (s.slice(6, 10));
+// console.log (s.slice(12));
+// console.log (s.slice(12, -10));
+
+// var arr = s.split(" ");
+
+// console.log (s.split());
+// console.log (s.substr(23, 9));
+// console.log (s.substr(23));
+
+// console.log (s.substring(5, 10));
+console.log (s. trim(","));
