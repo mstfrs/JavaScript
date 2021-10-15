4a43b722667f630c0cde7ee53f97347a19760a43
mstfrs <mstfrs68@gmail.com>
2021-10-15 20:52:56 +0300

    array Çalışması eklendi

---
+++ arrays/array.js
@@ -0,0 +1,77 @@
+// const car=["opel","audi","BMW",2,["Hello","World"]]
+// console.log(car);
+
+// const car2= new Array ("opel","audi","BMW",2,["Hello","World"])
+// console.log(car2);
+// const x= new Array(10);
+// console.log(x);
+// car[4]="Mercedes"
+// console.log(car);
+
+// const cars=["opel","audi","BMW"];
+// console.log(cars);
+// console.log(cars.length);
+
+// const cars2=["Renault", "ford"];
+
+// console.log(cars.concat(cars2));
+// console.log(cars+cars2);
+
+// const cars3=["opel","audi","BMW",2,["Hello","World"]]
+// console.log(cars2.concat(cars,cars3));
+// console.log(cars2+cars3);
+// console.log(cars3.sort());
+
+
+// const points = [40,100,1,5,25,10];
+
+// console.log(points.sort());
+// console.log(points.sort(function(a,b){return a-b}));
+
+
+// var alp="abcdefgğhıijklmnoöprsştuüvyz0123456789"
+
+
+// const fruits=["banana",,"orange","apple","mango"];
+// console.log(fruits);
+// fruits.push("pears","fig");
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+
+// console.log(fruits.sort());
+
+// var names=["john","Edward","Victor"];
+// var deleted=names.splice(1,2, "Mark","James")
+// console.log(deleted);
+// console.log(names);
+
+// console.log(names.splice(-2,2));
+// console.log(names);
+
+// const colors=["red","yellow","green","blue","pink","green"];
+// const x= colors.indexOf("green",3);
+// const x1=colors.lastIndexOf("green",3);
+
+// console.log(x);
+// console.log(x1);
+
+const colors = ["Red", "Yellow", "Green", "Blue", "Pink","Purple","Gray","lime"];
+// 1. for
+// for (let i = 0; i < colors.length;i++){
+//     console.log(colors[i])
+// };
+
+// for in
+// for (i in colors) console.log(colors[i]);
+
+//for each
+
+// colors.forEach(i=> console.log(i));
+
+//for of
+
+for (const i of colors) {console.log(i);
+    
+}
+
