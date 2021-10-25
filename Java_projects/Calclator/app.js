76fac8719a224eca36c7be67022ce344e8bd6e88
mstfrs <mstfrs68@gmail.com>
2021-10-25 17:05:57 +0300

    calculator dosyası yüklendi

---
+++ Java_projects/Calclator/app.js
@@ -0,0 +1,17 @@
+// let firstNumber = document.getElementsByClassName("inputs")[0];
+// console.log(firstNumber)
+
+// let firstNum = document.getElementById('calc-button');
+// console.log(firstNum);
+
+// let firstNameByClassName = document.getElementsByClassName('inputs')[0];
+// console.log(firstNameByClassName);
+
+// let idAttr = firstNum.getAttribute('id');
+// console.log(idAttr);
+
+
+
+
+let contdiv= document.getElementById("container");
+console.log(contdiv)
--- Java_projects/Fibanocci_Numbers/app.js
+++ Java_projects/Fibanocci_Numbers/app.js
@@ -10,17 +10,23 @@
     
 // }
 
-function fibonacci(x) {  
-let sum1=0,sum2=1,total=0;
-for (let i = 0; i < x; i++) {
-    total =sum1+sum2;
-    sum1=sum2;
-    sum2=total;     
-}
-console.log(total) 
-}
-fibonacci(7)
-fibonacci(12)
+// function fibonacci(x) {  
+// let sum1=0,sum2=1,total=0;
+// for (let i = 0; i < x; i++) {
+//     total =sum1+sum2;
+//     sum1=sum2;
+//     sum2=total;     
+// }
+// console.log(total) 
+// }
+// fibonacci(7)
+// fibonacci(12)
 
+// const x = 6 % 2;
+// const y = x ? 'One': 'Two';
 
+// console.log(y)
+// console.log(x)
 
+let firstNum = document.getElementById('firstnumber');
+console.log(firstNum);
