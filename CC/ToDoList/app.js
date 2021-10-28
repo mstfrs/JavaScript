08690a6a0c3143058a0b61d3b225342a79ad760a
mstfrs <mstfrs68@gmail.com>
2021-10-28 22:40:13 +0300

    ToDoList projesi tamamlandı

--- CC/ToDoList/app.js
+++ CC/ToDoList/app.js
@@ -1,58 +1,64 @@
-let enteredTask = document.getElementById("taskInput");
-let taskAddButton = document.getElementById("addButton")
-let taskList = document.getElementById("tasks")
-let todolistitem=document.getElementById("todoli")
-// console.log(enteredTask);
-// console.log(taskAdd);
-// console.log(taskList);
-var i=0;
+//Completed Tasks **********************************************
 
-taskAddButton.addEventListener("click", ()=>{
-    i +=1;
-    console.log(i);
-    let listItemDiv= document.createElement("div")
-    taskList.appendChild(listItemDiv)
+function completed() {
+    var listItems = document.getElementsByTagName("li");
+// console.log(listItems);
 
-    let liElement= document.createElement("li")
-    listItemDiv.appendChild(liElement)
-    listItemDiv.setAttribute("class","listitems")
+for (var i = 0; i<listItems.length;i++) {
 
-    liElement.setAttribute("class", "todoli")
-    let liElementById = document.getElementsByClassName("todoli");
-    // let x=liElementById.length;
-    console.log(liElementById[i]);
-    let doneButton=document.createElement("img")
-    listItemDiv.appendChild(doneButton);
-    doneButton.setAttribute("src","./img/finish.png")
-    let delButton=document.createElement("img")
-    listItemDiv.appendChild(delButton);
-    delButton.setAttribute("src","./img/del.jpg")
-    liElementById[i].innerText=enteredTask.value;
+    listItems[i].addEventListener("click", function () {
+        this.classList.toggle("completed");
+        
+    });  
+}
     
+}
+completed();
+
+//Completed Tasks **********************************************
+
+
+
+
+//Deleted Tasks **********************************************
+function deleted() {
+    var delbuttons = document.getElementsByTagName("span")
+
+for (var x = 0; x < delbuttons.length; x++) {
+    delbuttons[x].addEventListener("click",function(){
+        var deletedItem= this.parentElement;
+        deletedItem.remove();
+    })    
+}
+
+    
+ }
+
+deleted();
+//Deleted Tasks **********************************************
+ 
+
+var addedtask=document.getElementsByTagName("input")[0];
+
+addedtask.addEventListener("keypress", function(e){
+    if (e.keyCode===13) {
+        var newtask=document.createElement("li")
+        newtask.innerHTML=addedtask.value;
+        var newspan=document.createElement("span" )
+        var newIcon = document.createElement("i")
+        newIcon.setAttribute("class","fas fa-trash")
+
+        newspan.insertBefore(newIcon,newspan.firstChild);
+        newtask.insertBefore(newspan,newtask.firstChild);
+
+        var tasks=document.getElementsByTagName("ul")[0];
+        tasks.insertBefore(newtask,tasks.lastChild);
+
+        deleted();
+        completed();
+        addedtask.value="";
+
+         
+    }
+
 })
-
-
-
-
-// taskAdd.addEventListener("click", ()=>{
-//     let liElement= document.createElement("li");
-    
-//     let finishTask= document.createElement("img")
-//     let checkTask= document.createElement("input")
-//     liElement.setAttribute("class","todoli")
-//     checkTask.setAttribute("type","checkbox")
-//     checkTask.setAttribute("class","check")
-//     finishTask.setAttribute("src","./img/finish.png")
-    
-
-    
-    
-//     liElement.innerText=enteredTask.value;
-//     liElement.appendChild(checkTask);
-//     taskList.appendChild(liElement);
-//     liElement.appendChild(finishTask);
-    
-    
-
-
-//  })
