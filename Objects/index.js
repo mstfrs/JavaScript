03a3218728d01c8888e1f23b0c0a5bc70fdc76cd
mstfrs <mstfrs68@gmail.com>
2021-11-11 20:09:17 +0300

    ..

---
+++ Objects/index.js
@@ -0,0 +1,56 @@
+console.log('» index.js is running');
+
+// const person = {
+//   firstName: 'John',
+//   age: 30,
+//   location: {
+//     city: 'New York',
+//     temp: 92,
+//   },
+//   2021: 'Clarusway',
+// };
+
+// console.log(person.firstName);
+// console.log(person['age']);
+// console.log(person);
+// console.log(person.lastName);
+
+// person.lastName = 'Smith';
+// console.log(person['midName']);
+
+// console.log(person);
+// console.log(person['2021']);
+
+// const field = 'Name';
+// console.log(person['first' + field]);
+
+///////*******methods */
+
+const person = {
+  firstName: 'John',
+  lastName: 'Smith',
+  age: 30,
+  citiesLived: ['New York', 'Paris', 'London'],
+  location: {
+    city: 'New York',
+    temp: 92,
+  },
+  2021: 'Clarusway',
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  getFullName2() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  aMethod: () => {
+    return 'Hello';
+  },
+  printCitiesLived() {
+    this.citiesLived.forEach(city => {
+      console.log(this.firstName + ' has lived in ' + city);
+    });
+  },
+};
+// console.log(person.getFullName2());
+
+person.printCitiesLived();
